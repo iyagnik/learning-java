230406016215627692dbd2f25e031d856b589251
yagp-odoo <yagp@odoo.com>
2024-07-15 18:30:44 +0530

    First Session of Java
    The Legendary Hello Word

---
+++ s1_begin.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Your Name:");
+    String name = sc.nextLine();
+    System.out.println("Hello " + name);
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
